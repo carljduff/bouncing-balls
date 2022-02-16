836dc6724ebeee31bfc0d0b186572d3961dff047
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-02-16 06:38:42 +0000

    update

--- js/main.js
+++ js/main.js
@@ -35,5 +35,68 @@ class Ball {
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI); //traces an arc shape on the paper. X & Y position of the arc's center, specifying their properties. The radius is the ball's size property. The last two parameters specify the start and end number of degrees around the circle that the arc is drawn betwee. 0 degrees and 2 * PI is 360 degrees in radians which is a circle. 
         ctx.fill(); // fills the area of the circle with the color specified in fillStyle.
     }
+
+    update() {
+        if ((this.x + this.size) >= width) {
+            this.velX = -(this.velX);
+        }
+
+        if ((this.x - this.size) <= 0) {
+            this.velX = -(this.velX);
+        }
+
+        if ((this.y + this.size) >= height) {
+            this.velY = -(this.velY);
+        }
+
+        if ((this.y - this.size) <= 0) {
+            this.velY = -(this.velY);
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    collisionDetect() {
+        for (const ball of balls) {
+            if (!(this === ball)) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + ball.size) {
+                    ball.color = this.color = randomRGB();
+                }
+            }
+        }
+    }
 }
 
+const balls = [];
+
+while (balls.length < 25) {
+    const size = random(10,20);
+    const ball = new Ball(
+        random(0 + size,width - size),
+        random(0 + size,height - size),
+        random(-7, 7),
+        random(-7, 7),
+        randomRGB(),
+        size
+    );
+    balls.push(ball);
+}
+
+function loop() {
+    ctx.fillStyle = 'rgba(0,0,0,0.25)';
+    ctx.fillRect(0, 0, width, height);
+
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
+    }
+    requestAnimationFrame(loop);
+}
+
+loop();
